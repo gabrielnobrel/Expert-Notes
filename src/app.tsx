c200e4345e8d8a3099ddcfd3bc0018c095ac2978
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2024-02-22 20:33:18 -0300

    primeiro commit

---
+++ src/app.tsx
@@ -0,0 +1,87 @@
+import { ChangeEvent, useState } from "react"
+import logo from "./assets/logo-nlw-expert.svg"
+import { NewNoteCard } from "./components/new-note-card"
+import { NoteCard } from "./components/note-card"
+
+interface Note {
+    id: string,
+    date: Date,
+    content: string
+}
+
+export function App() {
+    const [search, setSearch] = useState('')
+
+    const [notes, setNotes] = useState<Note[]>(() => {
+        //cria a função para ele buscar a nota salva no localstorage
+        const notesOnStorage = localStorage.getItem('notes')
+
+        if (notesOnStorage) {
+            return JSON.parse(notesOnStorage)
+        }
+
+        return []
+    })
+
+    function onNoteCreated(content: string) {
+        const newNote = {
+            id: crypto.randomUUID(), //ele gera um id único
+            date: new Date(),
+            content,
+        }
+
+        const notesArray = [newNote, ...notes]
+
+        setNotes(notesArray)
+
+        //armazena no local storage
+        localStorage.setItem('notes', JSON.stringify(notesArray))
+    }
+
+    function onNoteDeleted(id: string) {
+        const notesArray = notes.filter(note => {
+            return note.id != id
+        })
+
+        setNotes(notesArray)
+
+        localStorage.setItem('notes', JSON.stringify(notesArray))
+    }
+
+    function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+        const query = event.target.value
+
+        setSearch(query)
+    }
+
+    // A busca fica case sensitiva, podendo pesquisar com letras minúsculas ou maiúsculas
+    const filteredNote = search != '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes
+
+    return (
+        <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
+            <img src={logo} alt="NLW Expert" />
+
+            <form className="w-full">
+                <input
+                    type="text"
+                    placeholder="Busque em suas notas..."
+                    className="w-full bg-transparent text-3xl font-semibold outline-none tracking-tighter placeholder:text-slate-500"
+                    onChange={handleSearch}
+                />
+            </form>
+
+            <div className="h-px bg-slate-700" />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-cols-3 gap-6 auto-rows-[250px]">
+                <NewNoteCard onNoteCreated={onNoteCreated} />
+
+                {
+                    filteredNote.map(note => {
+                        return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
+                    })
+                }
+            </div>
+        </div>
+
+    )
+}
